552783d90cba976b49aa42e877199520f8a8f6f8
Param <paramjeetsingh7023@gmail.com>
2023-07-28 15:50:59 +0530

    second

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,48 @@
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css'; // Import the date picker styles
+import axios from "axios"
+
 function App() {
+	const [startDate, setStartDate] = useState(null);
+	const [stockSymbol, setStockSymbol] = useState()
+	const [date, setDate] = useState()
+	console.log("s", stockSymbol)
+
+  const onSubmit = () => {
+    const config = {
+      stockSymbol : stockSymbol,
+      date: date
+    }
+    const url = ""
+    axios.post().then(()=>{
+
+    }).catch((err)=>{
+      console.log(err)
+    })
+
+  }
+
 	return (
-		<h1>Hello world</h1>
+		<div style={{display: "flex",
+			justifyContent: "center",
+			alignItems: "center",
+			height: "100vh"}}>
+      <div style={{ border: "1px solid #ccc",
+  padding: "20px",
+  textAlign: "center"}}>
+        <h2>Card Title</h2>
+        <input type="text" onChange={(e)=>e.target.value}  value={stockSymbol} placeholder="Stock Symbol" style={{marginTop:"5px", marginBottom:"10px"}} /><br></br>
+        <DatePicker
+        value ={date}
+		    style={{marginTop:"10px"}}
+          selected={startDate}
+          onChange={(date) => setStartDate(date)}
+          placeholderText="Select a date" // Placeholder for the input
+        /><br></br>
+        <button style={{marginTop:"5px"}}>Submit</button>
+      </div>
+    </div>
 	);
 }
 
--- server/app.js
+++ server/app.js
@@ -26,9 +26,35 @@ app.use(bodyParser.json());
 
 app.enable('trust proxy');
 
-app.post('/api/fetchStockData', (req, res) => {
-    // YOUR CODE GOES HERE, PLEASE DO NOT EDIT ANYTHING OUTSIDE THIS FUNCTION
+app.get('/api/fetchStockData', (req, res) => {
+const polygonApiKey = 'Cuy2hjZ5kQQo23SzedEcN8FMbAnVYTaU';
+
+const stockSymbol = 'AAPL';
+const date = '2023-07-26';
+
+const apiUrl = `https://api.polygon.io/v1/open-close/${stockSymbol}/${date}?apiKey=${polygonApiKey}`;
+
+
+axios.get(apiUrl)
+  .then(response => {
+    console.log("data", response.data)
+    
+    // Extract trade statistics from the response
+    const {open, high, low, close, volume } = response.data;
+
+    // Log the trade statistics
+    console.log('Open Price:', open);
+    console.log('High Price:', high);
+    console.log('Low Price:', low);
+    console.log('Close Price:', close);
+    console.log('Volume:', volume);
+  })
+  .catch(error => {
+    console.error('Error fetching trade statistics:', error.message);
+  });
+
     res.sendStatus(200);
+    res.send({open, high, low, close, volume})
 });
 
 const port = process.env.PORT || 5000;
